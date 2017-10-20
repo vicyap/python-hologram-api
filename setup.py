070e8fd34bf9f4a718460ae254e666ee3cdea578
Victor Yap <victor@vicyap.com>
2017-10-20 15:12:39 +0000

    add requests to setup.py, clean up metadata

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'requests',
 ]
 
 setup_requirements = [
@@ -32,7 +32,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author="Victor Yap",
     author_email='victor@vicyap.com',
-    url='https://github.com/vicyap/python_hologram_api',
+    url='https://github.com/vicyap/python-hologram-api',
     packages=find_packages(include=['python_hologram_api']),
     include_package_data=True,
     install_requires=requirements,
@@ -45,12 +45,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements,
