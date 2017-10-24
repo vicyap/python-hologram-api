b0f0b7e419c37acd97a29da4c54737e1a8550202
Victor Yap <victor@vicyap.com>
2017-10-24 04:34:12 +0000

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='python_hologram_api',
-    version='0.1.2',
+    version='0.1.3',
     description="Python client for https://dashboard.hologram.io/api.",
     long_description=readme + '\n\n' + history,
     author="Victor Yap",
